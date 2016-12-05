7c303ccb10cc760d20b7155e348c4259ec9e8038
Ruben Amortegui <ruben.amortegui@gmail.com>
2016-12-04 22:05:53 -0700

    Filter calculate parameter to receive only positive integers.

--- lib/factorial.ex
+++ lib/factorial.ex
@@ -1,8 +1,8 @@
 defmodule Factorial do
-  def calculate(x) when is_integer(x) and x == 0 do 
+  def calculate(x) when is_integer(x) and x == 0 and x >= 0 do
     1 
   end
-  def calculate(x) when is_integer(x) do 
+  def calculate(x) when is_integer(x) and x >= 0 do
     x * calculate(x-1) 
   end
 end
